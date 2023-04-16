aebf79562a7fdc079f2b1f2c12055df75122bf61
Vaibhavbajoriya <vaibhavbajoriya1234@gmail.com>
2023-04-16 18:51:58 +0530

    created a new project

---
+++ src/constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-1.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-2.jpg',
+    title: 'The Metropolis',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-3.jpg',
+    title: 'The WonderLand',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-4.jpg',
+    title: 'The Jurassic World',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-5.jpg',
+    title: 'The HogWards',
+  },
+];
+
+export const startingFeatures = [
+  'First select the world you want to explore ',
+  'Click the VR Button ',
+  'Enjoy the Imersive World of Imagination',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'The launch of the MyVerse makes a Step Forward to New Social world',
+    subtitle:
+        '',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the madness of the MyVerse',
+    subtitle:
+        '',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        '',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
---
+++ src/sections/About.jsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About MyVerse" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">MyVerse</span> is the clone of Metaverse.
+        is a virtual reality simulation that can be used for various purposes, from entertainment and social networking to
+        education and business. It is a 3D virtual world created and maintained by users and can be explored and interacted 
+        with just like the physical world. Let's{' '}<span className="font-extrabold text-white">explore </span>
+        MyVerse by scrolling down
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
